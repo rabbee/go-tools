b6939a6150a1c5d56e6715af0382aa775d864657
Rabbee <windrabbee@outlook.com>
2019-10-09 23:16:46 +0800

    pearson correlation finished

--- score/simple.go
+++ score/simple.go
@@ -2,46 +2,51 @@ package score
 
 import "math"
 
-func InvalidVector(a, b Vector) bool {
-	if len(a.Values) == 0 || len(b.Values) == 0 || len(a.Values) != len(b.Values) {
+func InvalidVector(x, y Vector) bool {
+	if len(x.Values) == 0 || len(y.Values) == 0 || len(x.Values) != len(y.Values) {
 		return true
 	}
 	return false
 }
 
-//EuclideanDistance 欧几里得距离评价 [0-1]
-func EuclideanDistance(a, b Vector) float64 {
-	if InvalidVector(a, b) {
+//EuclideanDistance 欧几里得距离评价 [0, 1]
+func EuclideanDistance(x, y Vector) float64 {
+	if InvalidVector(x, y) {
 		return 0
 	}
 	var sum float64 = 0
-	for index := 0; index < len(a.Values); index++ {
-		sum += math.Pow(a.Values[index]-b.Values[index], 2)
+	for index := 0; index < len(x.Values); index++ {
+		sum += math.Pow(x.Values[index]-y.Values[index], 2)
 	}
 	return 1 / (1 + math.Sqrt(sum))
 }
 
-//PearsonCorrelation 皮尔逊相关度评价 [0-1]
+//PearsonCorrelation 皮尔逊相关度评价 [-1, 1]
 //和平均水平偏离很大
-//TODO: 未完成
-func PearsonCorrelation(a, b Vector) float64 {
-	if InvalidVector(a, b) {
+func PearsonCorrelation(x, y Vector) float64 {
+	if InvalidVector(x, y) {
 		return 0
 	}
 	var (
-		sumA, sumB, sumASq, sumBSq, pSum float64
+		sumX, sumY, sumXSq, sumYSq, pSum float64 = 0, 0, 0, 0, 0
+		length                                   = len(x.Values)
 	)
-	for index := 0; index < len(a.Values); index++ {
-		sumA += a.Values[index]
-		sumASq += math.Pow(a.Values[index], 2)
-		sumB += b.Values[index]
-		sumBSq += math.Pow(b.Values[index], 2)
-		pSum += a.Values[index] * b.Values[index]
+	for index := 0; index < length; index++ {
+		//简单和
+		sumX += x.Values[index]
+		sumY += y.Values[index]
+		//平方和
+		sumXSq += math.Pow(x.Values[index], 2)
+		sumYSq += math.Pow(y.Values[index], 2)
+		//乘积和
+		pSum += x.Values[index] * y.Values[index]
 	}
-	den := math.Sqrt((sumASq - math.Pow(sumA, 2)/float64(len(a.Values))) *
-		(sumBSq - math.Pow(sumB, 2)/float64(len(b.Values))))
+	den := math.Sqrt(
+		(sumXSq - math.Pow(sumX, 2)/float64(length)) *
+			(sumYSq - math.Pow(sumY, 2)/float64(length)),
+	)
 	if den == 0 {
 		return 0
 	}
-	return (pSum - (sumA * sumB / float64(len(a.Values)))) / den
+	return (pSum - (sumX * sumY / float64(length))) / den
 }
--- score/simple_test.go
+++ score/simple_test.go
@@ -5,36 +5,28 @@ import (
 	"testing"
 )
 
+var (
+	p1 = score.Vector{
+		Values: []float64{
+			4.5,
+			8,
+		},
+	}
+
+	p2 = score.Vector{
+		Values: []float64{
+			4,
+			4,
+		},
+	}
+)
+
 func TestEuclidean(t *testing.T) {
-	p1 := score.Vector{
-		Values: []float64{
-			1.99,
-			1.01,
-		},
-	}
-	p2 := score.Vector{
-		Values: []float64{
-			2,
-			1,
-		},
-	}
 	d := score.EuclideanDistance(p1, p2)
 	t.Log(d)
 }
 
 func TestPearsonCorrelation(t *testing.T) {
-	p1 := score.Vector{
-		Values: []float64{
-			1.99,
-			1.01,
-		},
-	}
-	p2 := score.Vector{
-		Values: []float64{
-			2,
-			1,
-		},
-	}
 	d := score.PearsonCorrelation(p1, p2)
 	t.Log(d)
 }
